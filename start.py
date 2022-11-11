81a38b20deca7ca11320132baa830dc304fd2966
WandsMire <reza@mirwanda.com>
2022-11-11 18:54:44 +0900

    Update start.py

--- start.py
+++ start.py
@@ -118,8 +118,8 @@ with dataset_cont:
     st.markdown("The ML model was trained on the dataset below (adopted from [Applied Predictive Modeling](https://link.springer.com/book/10.1007/978-1-4614-6849-3)). "
              "The last column (churn) is the target showing whether the customer unsubscribed from the service (yes) or not (no).")
     df = get_data()
-    df = df.drop("", axis=1)
-    
+    #df = df.drop("", axis=1)
+    df = df.drop(columns=[0], axis=1, inplace=True)
     st.dataframe(df)
 
 
